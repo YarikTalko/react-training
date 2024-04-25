9e424116287da28cbae89c6655e1b4119bfc7531
YarikTalko <ariktalko@gmail.com>
2024-04-25 17:43:28 +0300

    add components train

--- src/index.js
+++ src/index.js
@@ -1,45 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom/client"
-import App from "./App"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Car from "./Car";
 
-// const myElement = (
-//     <table>
-//         <tr>
-//             <th>Name</th>
-//         </tr>
-//         <tr>
-//             <td>John</td>
-//         </tr>
-//         <tr>
-//             <td>Elsa</td>
-//         </tr>
-//     </table>
-// );
-
-// const myElement = <h1>I LOVE {5 + 5} JSX</h1>;
-
-// const myElement = (
-//     // <div>
-//     <>
-//         <p>iaeutniu</p>
-//         <p>iaeutniu</p>
-//         {/*</div>*/}
-//     </>
-// );
-
-// const myElement = <input type="text"/>;
-
-// const myElement = <h1 className="myClass">Boo</h1>;
-
-const x = 5;
-// let text = "Goodbye";
-// if (x < 10) {
-//     text = "Hello";
-// }
-
-// const myElement = <h1>{text}</h1>;
-// const myElement = <h1>{(x) < 10 ? "Hello" : "Goodbye"}</h1>;
+function Garage() {
+    return (
+        <>
+            <h1>Who lives in my Garage?</h1>
+            <Car/>
+        </>
+    );
+}
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
+root.render(<Car />);
