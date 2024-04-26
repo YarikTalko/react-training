4ab3ee50d99f9fc79868cc314492a5628cee132f
YarikTalko <ariktalko@gmail.com>
2024-04-26 19:07:24 +0300

    add props train

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Car from "./Car";
+
+function Car(props) {
+    return <h2>I am a {props.brand.model}!</h2>;
+}
 
 function Garage() {
+    // const carName = "Ford";
+    const carInfo = {name: "Ford", model: "Mustang"};
     return (
         <>
-            <h1>Who lives in my Garage?</h1>
-            <Car/>
+            <h1>Who lives in my garage?</h1>
+            <Car brand={carInfo}/>
         </>
     );
 }
 
+// const myElement = <Car brand="Ford"/>;
+
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<Car />);
+root.render(<Garage/>);
