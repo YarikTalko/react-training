58f1bf5070b4527d0a6b8500016634df464e2a07
YarikTalko <ariktalko@gmail.com>
2024-04-27 10:41:15 +0300

    add conditionals train

--- src/index.js
+++ src/index.js
@@ -1,18 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-function Football() {
-    const shoot = (a, b) => {
-        alert(b.type)
-    }
-
-    return (
-        <button onClick={(event) => shoot("Goal!", event)}>
-            Take the shot!
-        </button>
-    );
+function MissedGoal() {
+    return <h1>Missed!</h1>;
 }
+
+function MadeGoal() {
+    return <h1>Goal!</h1>;
+}
+
+function Goal(props) {
+    const isGoal = props.isGoal;
+    // if (isGoal) {
+    //     return <MadeGoal/>;
+    // }
+    // return <MissedGoal/>;
+    return (
+        <>
+            {isGoal ? <MadeGoal/> : <MissedGoal/>}
+        </>
+    )
+}
+
+// function Garage(props) {
+//     const cars = props.cars;
+//     return (
+//         <>
+//             <h1>Garage</h1>
+//             {
+//                 cars.length > 0 &&
+//                 <h2>
+//                     You have {cars.length} cars in your garage.
+//                 </h2>
+//             }
+//         </>
+//     );
+// }
+
+// const cars = ["Ford", "Ferrari", "Audi", "BMW"];
+// const cars = [];
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<Football/>);
+root.render(<Goal isGoal={true}/>);
+// root.render(<Garage cars={cars}/>);
